88cc44b13a323bda119e55e21063c8b5c0d6d32d
ekalaya <ekalaya.manullang@karyamas-group.com>
2024-03-02 00:43:56 +0700

    lupa route logout

--- routes/api.php
+++ routes/api.php
@@ -20,6 +20,8 @@ Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
 
 // login api
 Route::post('/login', [\App\Http\Controllers\Api\AuthController::class, 'login']);
+Route::post('/logout', [\App\Http\Controllers\Api\AuthController::class, 'logout'])->middleware('auth:sanctum');
+
 Route::apiResource('/api-products', \App\Http\Controllers\Api\ProductController::class)->middleware('auth:sanctum');
 Route::apiResource('/api-categories', \App\Http\Controllers\Api\CategoryController::class)->middleware('auth:sanctum');
 
